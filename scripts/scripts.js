ddb767459e6ecf69ee130fb5cda2d5baa55ef440
Arend <arend3571@hotmail.com>
2022-03-05 20:33:48 +0100

    ensureplayvideo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,14 @@
-$(document).ready(function(){
-});
+function ensurePlayVideo(selector){
+  var video = document.querySelector(selector);
+  var promise = video.play();
+
+  if (promise !== undefined) {
+    promise.then(_ => {
+      // Autoplay started!
+
+    }).catch(error => {
+      // Show something in the UI that the video is muted
+      video.muted = true;
+    });
+  }
+}
